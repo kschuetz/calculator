cf89524f044cd0653c971e3e3601b9946775fc0d
Kevin Schuetz <schuetzk@gmail.com>
2020-05-10 22:32:34 -0500

    Use type guard functions

--- src/core/util/nonEmptyUtils.ts
+++ src/core/util/nonEmptyUtils.ts
@@ -3,8 +3,8 @@ import {Option} from "fp-ts/es6/Option";
 import * as Immutable from "immutable";
 
 export function maybeNonEmptyArray<A>(xs?: A[] | Immutable.List<A> | Immutable.Set<A>): Option<NonEmptyArray<A>> {
-    if (xs instanceof Immutable.List || xs instanceof Immutable.Set) {
-        return fromArray((xs as Immutable.List<A> | Immutable.Set<A>).toArray());
+    if (Immutable.List.isList(xs) || Immutable.Set.isSet(xs)) {
+        return fromArray(xs.toArray());
     } else {
         return fromArray(xs as Array<A>);
     }
